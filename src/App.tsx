751c886753687f75c0a385f1e59533356535fe75
asraful <asrafulislamrayhan2@gmail.com>
2024-06-02 16:13:00 +0600

    configured tailwindcss and shadcn ui

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="app p-10 bg-gray-20 ">
+      <Button>CLICK</Button>
+    </div>
+  );
+};
+
+export default App;
